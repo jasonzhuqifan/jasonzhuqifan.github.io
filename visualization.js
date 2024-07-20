730e33bc4071bc81b5049661396cf5b117f52ea4
Jason Zhu <jasonzhu000@hotmail.com>
2024-07-20 17:31:51 -0400

    scene 2

--- visualization.js
+++ visualization.js
@@ -164,10 +164,124 @@ d3.csv("us-states.csv").then(data => {
 
     // Scene 2: Peak and Decline in 2021
     function scene2() {
-        d3.select("#container").append("h1").text("Peak and Decline in 2021");
-        // Add visualization code for scene 2
+        d3.select("#container").html(""); // Clear the container
+        d3.select("#container").append("h1").text("COVID-19 Trends in 202111");
+        d3.select("#container").append("p").text("Key events and trends in the COVID-19 pandemic during 2021.");
+
+        // Set up the SVG and dimensions
+        const margin = {top: 20, right: 30, bottom: 40, left: 50};
+        const width = 800 - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        const svg = d3.select("#container").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        // Parse the date and set up scales
+        const parseDate = d3.timeParse("%Y-%m-%d");
+        const x = d3.scaleTime().range([0, width]);
+        const y = d3.scaleLinear().range([height, 0]);
+
+        // Load the data
+        d3.csv("us-states.csv").then(data => {
+            data.forEach(d => {
+                d.date = parseDate(d.date);
+                d.cases = +d.cases;
+                d.deaths = +d.deaths;
+            });
+
+            // Filter data for key states and for the year 2021
+            const keyStates = ["California", "Florida"];
+            const filteredData = data.filter(d => keyStates.includes(d.state) && d.date.getFullYear() === 2021);
+
+            // Nest data by state
+            const nestedData = d3.group(filteredData, d => d.state);
+
+            // Set the domains of the scales
+            x.domain(d3.extent(filteredData, d => d.date));
+            y.domain([0, d3.max(filteredData, d => d.cases)]);
+
+            // Create line generator
+            const line = d3.line()
+                .x(d => x(d.date))
+                .y(d => y(d.cases));
+
+            // Add the lines for each state
+            svg.selectAll(".line")
+                .data(nestedData)
+                .enter().append("path")
+                .attr("class", "line")
+                .attr("d", d => line(d[1]))
+                .style("stroke", (d, i) => d3.schemeCategory10[i])
+                .style("fill", "none");
+
+            // Add the X Axis
+            svg.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(x));
+
+            // Add the Y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y).ticks(10).tickFormat(d3.format("~s")));
+
+            // Manually add annotations
+            const annotations = [
+                { date: "2021-01-01", cases: 20000, label: "New Year surge", title: "January 2021" },
+                { date: "2021-09-01", cases: 150000, label: "Delta variant surge", title: "September 2021" }
+            ];
+
+            annotations.forEach(annotation => {
+                const xPos = x(parseDate(annotation.date));
+                const yPos = y(annotation.cases);
+
+                // Add annotation lines
+                svg.append("line")
+                    .attr("class", "annotation-line")
+                    .attr("x1", xPos)
+                    .attr("y1", yPos)
+                    .attr("x2", xPos - 50)
+                    .attr("y2", yPos - 50);
+
+                // Add annotation text
+                svg.append("text")
+                    .attr("class", "annotation")
+                    .attr("x", xPos - 55)
+                    .attr("y", yPos - 55)
+                    .text(annotation.title);
+
+                svg.append("text")
+                    .attr("class", "annotation")
+                    .attr("x", xPos - 55)
+                    .attr("y", yPos - 40)
+                    .text(annotation.label);
+            });
+
+            // Add a legend
+            const legend = svg.selectAll(".legend")
+                .data(keyStates)
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", (d, i) => d3.schemeCategory10[i]);
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(d => d);
+        });
+
         d3.select("#container").append("button").text("Next").on("click", nextScene);
     }
+
 
     // Scene 3: Variants and Continued Impact in 2022
     function scene3() {
